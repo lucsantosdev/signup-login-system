2831ae0d8e6e3dde45399da7c9b4888917b739da
Lucas Santos <lucsantosdev@gmail.com>
2024-06-30 14:38:49 -0300

    express application initialization and initial views/public files creation

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// importing dependencies modules
+const express = require('express')
+const path = require('path')
+const bcrypt = require('bcrypt')
+
+// creating an express application
+const app = express()
+
+// using EJS as the view engine
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render("login")
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+// choosing port: 3000
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server running on Port: ${port}`)
+})
